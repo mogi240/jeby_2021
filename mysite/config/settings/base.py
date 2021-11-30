df78a41fb64989f39399a70f0e6eba3d0e2c8b00
mogi240 <--list>
2021-11-30 14:16:27 +0900

    로그아웃 기능  개발 완료

--- mysite/config/settings/base.py
+++ mysite/config/settings/base.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "common",
+    "news",
 ]
 
 MIDDLEWARE = [
@@ -148,3 +149,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from news import views as views_news
 
 
 urlpatterns = [
+    path("", views_news.index),
     path("admin/", admin.site.urls),
     path("common/", include("common.urls")),
+    path("news/", include("news.urls")),
 ]
